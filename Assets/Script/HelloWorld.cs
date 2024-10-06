b326cb7cc2401435b5673d1574ad832ab04a9384
Tolga-dev <orhan.konat.12@gmail.com>
2024-10-06 14:10:40 +0300

    Init

--- Assets/Script/HelloWorld.cs
+++ Assets/Script/HelloWorld.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+namespace Script
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HelloWorld : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            Debug.Log("Hello World");
+        }
     }
 }
